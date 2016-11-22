58d744e9f90a164656af32936401b61bf3db6f7e
Pushpa <pushpap94@gmail.com>
2016-11-22 16:01:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pushpa/recyclerview/MyData.java
@@ -0,0 +1,17 @@
+package com.example.pushpa.recyclerview;
+
+/**
+ * Created by Pushpa on 10/25/2016.
+ */
+
+public class MyData {
+
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich","JellyBean", "Kitkat", "Lollipop", "Marshmallow"};
+    static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4", "5.0-5.1.1","6.0-6.0.1"};
+
+    static Integer[] drawableArray = {R.drawable.aries, R.drawable.taurus, R.drawable.gemini,
+            R.drawable.cancer, R.drawable.leo, R.drawable.virgo, R.drawable.libra,
+            R.drawable.scorpio, R.drawable.sagittarius, R.drawable.capricorn,R.drawable.aquarius, R.drawable.pisces};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11};
+}
